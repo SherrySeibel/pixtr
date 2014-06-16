3cb56edb4008b58ecdf376e23419a51c6ae0143e
Sherry Seibel <sherry.seibel@gmail.com>
2014-06-16 15:09:40 -0400

    Add create new gallery and strong params

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -2,4 +2,27 @@ class GalleriesController < ApplicationController
   def index
    @galleries = Gallery.all
   end
+
+  def show
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def new
+    @gallery = Gallery.new
+  end
+
+  def create
+    gallery = Gallery.create(gallery_params)
+    redirect_to "/galleries/#{gallery.id}"
+  end
+
+  private
+
+# This is creating STRONG params. The page can no longer be hacked.
+  def gallery_params
+    params.require(:gallery).permit(
+      :name,
+      :description,
+      )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
  get "/" => "galleries#index"
+ get "/galleries/new" => "galleries#new"
  get "/galleries/:id" => "galleries#show"
+ post "/galleries" => "galleries#create"
 end
