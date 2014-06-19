1f866a88bec3dc01ef9d15a189d3ce9c4f5853c5
Sherry Seibel <sherry.seibel@gmail.com>
2014-06-19 11:42:12 -0400

    Add database indexes on users and images

--- db/migrate/20140619151956_add_indexes.rb
+++ db/migrate/20140619151956_add_indexes.rb
@@ -1,4 +1,12 @@
 class AddIndexes < ActiveRecord::Migration
   def change
+    # Will disallow duplicate emails. 
+    # unique in a migration, uniqueness in a model (in model gives "pretty" errors). 
+    add_index :users, :email, unique: true 
+    # Table name, column name, index
+
+    # :gallery_id is a foreign key. It gets read a lot and written to very little.
+    # Therefore, we add an index.
+    add_index :images, :gallery_id
   end
 end
