44cc5c28a00b673c2a86e8a10a0a49c6a458d954
Sherry Seibel <sherry.seibel@gmail.com>
2014-06-19 11:05:47 -0400

    Add generated Monban files
    Also, show errors when user can't sign up

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true # Makes sure that 
+  # users can only have one email address in the database.
+  validates :password_digest, presence: true 
+  # pasword_digest makes sure the passwords are encrypted. 
+end
