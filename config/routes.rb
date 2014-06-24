d3bf4a2aac097108f3901233c3acb239196124ea
Sherry Seibel <sherry.seibel@gmail.com>
2014-06-24 10:28:04 -0400

    (Almost) working form for new groups

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   root to: "galleries#index"
+  resources :groups, only: [:new, :create]
   resources :galleries, only: [:show, :new, :create, :edit, :update, :destroy] do
     resources :images, only: [:new, :create, :destroy]
 
