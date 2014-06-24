a55530d1c713335e26f4487d445abe64fe8c7c87
Sherry Seibel <sherry.seibel@gmail.com>
2014-06-24 11:00:04 -0400

    Can add group descriptions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,25 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    group = Group.new(group_params)
+
+    if group.save
+      redirect_to :groups
+    else
+      render :new
+    end
+  end
+
+  def index
+    @groups = Group.all
+  end
+
+  private
+
+  def group_params
+    params.require(:group).
+      permit(:name, :description)
+  end
 end
