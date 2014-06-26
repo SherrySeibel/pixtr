e717e59e75214fd9f460639d8215f4c238017fdc
Sherry Seibel <sherry.seibel@gmail.com>
2014-06-26 16:41:27 -0400

    Users can tag images

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,6 +4,9 @@ class Image < ActiveRecord::Base # This is a model, representing an object in ou
 
   has_many :likes
 
+  has_many :tag_lists
+  has_many :tags, through: :tag_lists
+
   has_many :group_images
   has_many :groups, through: :group_images
 
